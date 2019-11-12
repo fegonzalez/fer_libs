a6587d5908fc09b2d05bcd874bbb94109afac8c3
swim <swim@tent.(none)>
2019-11-12 12:22:08 +0100

    valgrind foc updated

--- howtos/tools/valgrind/script_examples/memcheck_to_each_test.sh
+++ howtos/tools/valgrind/script_examples/memcheck_to_each_test.sh
@@ -6,28 +6,100 @@
 
 VALGRIND_OPTIONS="--leak-check=full --leak-resolution=high --partial-loads-ok=yes --track-origins=yes --undef-value-errors=yes"
 
+## "deep" options
+#  --num-callers=40 # implicitly used with value default value = 12
+#  --show-reachable=yes
+
+
 START_DATE=$(date) # just to check the total execution time
 
+## @test Dijstra library
+#
+WORK_DIR=$PWD
+TEST_DIR="$WORK_DIR/../test/test_static_lib/"
+ALG_DIR="$WORK_DIR/../"
+
+
 ##------------------------------------------------------------------------------
 
-for test in $(grep BOOST_AUTO_TEST_CASE ../test/test.cc \
-   | sed 's/\/\/.*//g' \
-   | sed 's/.*(//g' \
-   | sed 's/)//g')
-do
-#    echo Executing: valgrind $test
+exec_memcheck()
+{
+  exec_tests
+  bring_results
+}
 
-    LOCAL_VALGRIND_OPTIONS="$VALGRIND_OPTIONS --log-file=vgrind.$test.out"
-#    echo LOCAL_VALGRIND_OPTIONS : $LOCAL_VALGRIND_OPTIONS
 
-    RUN_COMMAND="./FlightPlanTranslator_test --run_test=$test"
+##------------------------------------------------------------------------------
+
+exec_tests()
+{
+    exec_Dijkstra_check
+
+#    exec_Boost_check()
+}
+
+##------------------------------------------------------------------------------
+
+exec_Dijkstra_check()
+{    
+    cd $ALG_DIR
+    make
+
+
+    cd $TEST_DIR
+#make run.concrete_singledist_test
+#echo $PWD
+    for test in $(ls *_test.cc | sed 's/.cc//g')
+    do
+	echo Executing: valgrind $test
+
+	LOCAL_VALGRIND_OPTIONS="$VALGRIND_OPTIONS --log-file=vgrind.$test.out"
+#  echo LOCAL_VALGRIND_OPTIONS : $LOCAL_VALGRIND_OPTIONS
+
+#    RUN_COMMAND="make runtest"
+	RUN_COMMAND="make run.$test"
 #    echo RUN_COMMAND : $RUN_COMMAND 
         
-   valgrind $LOCAL_VALGRIND_OPTIONS $RUN_COMMAND
+	valgrind $LOCAL_VALGRIND_OPTIONS $RUN_COMMAND
 
-done 
+    done 
+}
 
 ##------------------------------------------------------------------------------
+
+bring_results()
+{
+    bring_results_Dijkstra_check
+}
+
+##------------------------------------------------------------------------------
+
+bring_results_Dijkstra_check()
+{
+    mv $TEST_DIR/*.out $WORK_DIR
+    cd $WORK_DIR
+
+# echo
+# echo
+# echo --- TEST_DIR ---
+#   pwd
+#   echo $TEST_DIR
+#   ls $TEST_DIR
+# echo
+# echo --- WORK_DIR ---
+#   echo $WORK_DIR
+#   ls $WORK_DIR
+# echo
+#   exit 44
+  
+}
+
+
+##------------------------------------------------------------------------------
+
+show_results()
+{
+cd $WORK_DIR
 
 echo
 echo
@@ -46,3 +118,13 @@ echo
 echo Memory leaks:
 grep -L "definitely lost: 0" *.out
 echo
+
+}
+
+##==============================================================================
+## main script
+
+exec_memcheck
+show_results
+
+##==============================================================================
